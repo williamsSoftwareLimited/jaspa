9ea7ce271b6b15a6929a00562b27415b91a71531
gavin <gavin@codebehind.org>
2026-02-23 18:58:34 +0000

    removed pointless styling and tidied a little

--- codebehind.js
+++ codebehind.js
@@ -7,17 +7,6 @@ class Codebehind extends HTMLElement {
     //const shadow = this.attachShadow({ mode: "open" });
     const style = document.createElement("style");
 
-    style.textContent=
-      `code-behind {
-        font: sans-serif bold 50px;
-        display: flex;
-        flex: 1;
-        align-items: center;
-        justify-content: center;
-        color: #112;
-        font: bold 5rem 'Raleway';
-        text-shadow: #333 1px 0 10px;
-      }`;
       //shadow.appendChild(style);
   }
 }
